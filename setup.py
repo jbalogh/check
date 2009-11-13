c1b3234039c034da81a7e3e13c29fab8426f405b
Jeff Balogh <me@jeffbalogh.org>
2009-11-12 21:38:55 -0800

    replacing pep8.py file with a package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='check',
-      version='0.3',
-      scripts=['check.py', 'pep8.py'],
-      install_requires=['pyflakes'],
+      version='0.4pre',
+      scripts=['check.py'],
+      install_requires=['pyflakes', 'pep8'],
 )
